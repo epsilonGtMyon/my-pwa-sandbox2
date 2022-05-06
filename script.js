da32e9af2271e6ad5aa2b8eea6d785a7fe10f5d8
epsilonGtMyon <epsilon.87gmaigt@gmail.com>
2022-05-06 17:12:46 +0900

    xxx

--- script.js
+++ script.js
@@ -4,4 +4,17 @@
   const versionElem = document.querySelector("#version");
 
   versionElem.textContent = version;
+
+  //--------------------------
+
+  if ("serviceWorker" in navigator) {
+    const isLocal = location.hostname === "localhost";
+    if (isLocal) {
+      navigator.serviceWorker.register("./sw.js");
+    } else {
+      navigator.serviceWorker.register("./sw.js", {
+        scope: "/my-pwa-sandbox2/",
+      });
+    }
+  }
 })();
