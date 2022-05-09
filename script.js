a4c316495bf31fe48674c86f128d585d84e26627
epsilonGtMyon <epsilon.87gmaigt@gmail.com>
2022-05-09 23:23:53 +0900

    試行錯誤

--- script.js
+++ script.js
@@ -1,20 +1,7 @@
 (function () {
-  const version = "2";
+  const version = "1";
 
   const versionElem = document.querySelector("#version");
-
   versionElem.textContent = version;
 
-  //--------------------------
-
-  if ("serviceWorker" in navigator) {
-    const isLocal = location.hostname === "localhost";
-    if (isLocal) {
-      navigator.serviceWorker.register("./sw.js");
-    } else {
-      navigator.serviceWorker.register("./sw.js", {
-        scope: "/my-pwa-sandbox2/",
-      });
-    }
-  }
 })();
